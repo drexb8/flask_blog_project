15d376c2a83f5639e59ac7c5f730f04058d1d313
Jardrex Bolon <drex.bolon@gmail.com>
2023-11-22 08:48:30 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
