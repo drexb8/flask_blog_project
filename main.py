ccfb39607d39eb691c4fe769858f1b85a0b55389
Jardrex Bolon <drex.bolon@gmail.com>
2023-11-23 09:28:09 +0800

    added name.html, user.html

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'justasecretkey39857238986'
+
+
+#name form class
+class NameForm(FlaskForm):
+    name = StringField('Your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
@@ -8,9 +19,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 
-
+@app.route('/name')
+def name():
+    form = NameForm()
+    return render_template('name.html', form=form)
 
 
 
